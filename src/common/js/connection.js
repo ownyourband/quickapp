de21cbb4153ab19d5bc516a77742439fd3df8cd3
Yumi <66252973+itsyumiki@users.noreply.github.com>
2026-09-25 15:11:11 +0300

    More tests about interconnect

--- src/common/js/connection.js
+++ src/common/js/connection.js
@@ -1,3 +1,37 @@
-import interconnect from "@system.interconnect";
+export function getConnectionState(connection) {
+  let connectionState = "disconnected";
+  console.log("the passed connection object:", connection);
 
-export async function getStatus() {}
+  connection.getReadyState({
+    success: (status) => {
+      console.log("connection success object", status);
+      connectionState = status;
+    },
+    fail: (message, code) => {
+      console.log("connection fail object", message, code);
+      connectionState = `BALL EXPOLOSTION: ${code}: ${message}`;
+    },
+  });
+  console.log("connection state object", connectionState);
+
+  return connectionState;
+}
+export function sendMessage(connection, message) {
+  console.log("got connection object in sendMessage:", connection);
+  console.log("message to send:", message);
+  connection.send({
+    data: {
+      str: "test",
+      num: 123,
+      message: message,
+    },
+    success: () => {
+      console.log(`handling success`);
+    },
+    fail: (data, code) => {
+      console.log(
+        `handling fail, errMsg = ${data.data}, errCode = ${data.code}`,
+      );
+    },
+  });
+}
